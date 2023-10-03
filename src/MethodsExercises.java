90ce9457df38b5027465142fecbd2dddccf9eca1
kennethhowell <howell.kenneth.a@gmail.com>
2023-10-03 11:51:07 -0500

    refactored tested and verified working methods to point

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -10,7 +10,7 @@ public class MethodsExercises {
 
         return x - y;
     }
-    public static int multiplaction(int x, int y){
+    public static int multiplication(int x, int y){
 
         return x * y;
     }
@@ -26,10 +26,10 @@ public class MethodsExercises {
 
     //Multiply without using operator
 
-    public static int multiplyLoop(int x){
+    public static int multiplyLoop(int x, int y){
         int bucket = 0;
 
-        for (int i = 0; i < x; i++) {
+        for (int i = 0; i < y; i++) {
             bucket += x;
         }
 
@@ -38,28 +38,18 @@ public class MethodsExercises {
 
     // recursion multiply bonus
 
-    public static int recursionMultiply(int operand, int count, int bucket){
+    public static int recursionMultiply(int x, int y, int bucket){
 
-        if(count == 0){
+        if(y == 0){
             return bucket;
         }
 
-        bucket += operand;
+        bucket += x;
 
-        count--;
+        y--;
 
-        return recursionMultiply(operand, count, bucket);
+        return recursionMultiply(x, y, bucket);
     }
-
-//    Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given range. If not, prompt the user to input their number again until the input is within range.
-//The method signature should look like this:
-//public static int getInteger(int min, int max);
-//and is used like this:
-//System.out.print("Enter a number between 1 and 10: ");
-//int userInput = getInteger(1, 10);
-//If the input is invalid, prompt the user again.
-//
-//Hint: recursion might be helpful here!
 
     public static int getInteger(int min, int max){
         System.out.printf("Please enter a number between %d and %d%n", min, max);
@@ -77,17 +67,6 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 
-        System.out.println(addition(2, 4));
-        System.out.println(subtraction(10, 5));
-        System.out.println(multiplaction(10, 3));
-        System.out.println(division(12, 3));
-        System.out.println(modulo(15, 4));
-
-        System.out.println(multiplyLoop(4));
-        System.out.println(recursionMultiply(4, 4, 0));
-        System.out.println(recursionMultiply(10, 10, 0));
-
-        System.out.println(getInteger(40, 45));
     }
 
 
